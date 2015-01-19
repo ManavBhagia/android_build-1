411b03fd963dcbdb4cfeb7922c59011e99592db2
ZION959 <ziontran@gmail.com>
2015-01-19 12:01:40 -0700

    dumpvar: add FLOOP_NEST_OPTIMIZE info

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -139,6 +139,11 @@ $(info   TARGET_USE_PIPE=$(TARGET_USE_PIPE))
 else
 $(info   TARGET_USE_PIPE=No)
 endif
+ifdef FLOOP_NEST_OPTIMIZE
+$(info   FLOOP_NEST_OPTIMIZE=$(FLOOP_NEST_OPTIMIZE))
+else
+$(info   FLOOP_NEST_OPTIMIZE=No)
+endif
 $(info ============================================)
 $(info ============================================)
 $(info   GCC_VERSION_ROM=$(TARGET_GCC_VERSION_AND))
