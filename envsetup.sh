253c8e3a037188f5bd2045e20665c3a2ef6b781e
JoseGalRe <josegalre@gmail.com>
2015-03-03 14:36:10 -0700

    build: optimizations for devices with low-RAM [1/2]
    Change-Id: I339042fc3d5b072297e4748be71ad6770367917d
    
    Conflicts:
    	envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -144,10 +144,12 @@ function setpaths()
     gccprebuiltdir=$(get_abs_build_var ANDROID_GCC_PREBUILTS)
 
     # defined in core/config.mk
+    cmremixlowramdevice=$(get_build_var CMREMIX_LOW_RAM_DEVICE)
     targetgccversionand=$(get_build_var TARGET_GCC_VERSION_AND)
     export TARGET_GCC_VERSION_AND=$targetgccversionand
     targetgccversion2=$(get_build_var 2ND_TARGET_GCC_VERSION)
     export TARGET_GCC_VERSION=$targetgccversion
+    export CMREMIX_LOW_RAM_DEVICE=$cmremixlowramdevice
 
     # The gcc toolchain does not exists for windows/cygwin. In this case, do not reference it.
     export ANDROID_TOOLCHAIN=
