3626d08fbf9b1effbe321cd38bead4857b32262d
DespairFactor <matthewalex@linux.com>
2016-10-06 03:31:29 -0400

    core: always apply O3 to sdclang

--- core/binary.mk
+++ core/binary.mk
@@ -359,10 +359,10 @@ my_target_global_ldflags := $($(LOCAL_2ND_ARCH_VAR_PREFIX)CLANG_TARGET_GLOBAL_LD
         SDCLANG_PRECONFIGURED_FLAGS :=
 
         ifeq ($(strip $(my_cc)),)
-            my_cc := $(SDCLANG_PATH)/clang $(SDLLVM_AE_FLAG) -Wno-vectorizer-no-neon
+            my_cc := $(SDCLANG_PATH)/clang $(SDLLVM_AE_FLAG) -Wno-vectorizer-no-neon -O3
         endif
         ifeq ($(strip $(my_cxx)),)
-            my_cxx := $(SDCLANG_PATH)/clang++ $(SDLLVM_AE_FLAG) -Wno-vectorizer-no-neon
+            my_cxx := $(SDCLANG_PATH)/clang++ $(SDLLVM_AE_FLAG) -Wno-vectorizer-no-neon -O3
         endif
     endif
 else
